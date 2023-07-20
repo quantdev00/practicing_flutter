9eaaa319555ad24a03499962b470e2cd13c430f5
Daniel Kiyiki <danielngolu154@gmail.com>
2023-07-20 22:23:13 +0200

    Added the model for the Icons and some svg img

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 40),
           Column(
             children: [
-              Padding(
+              const Padding(
                 padding: EdgeInsets.only(left: 20),
                 child: Text(
                   'Category',
@@ -52,7 +52,7 @@ class HomePage extends StatelessWidget {
           decoration: BoxDecoration(
             boxShadow: [
               BoxShadow(
-                color: Color(0xff1D1617).withOpacity(0.11),
+                color: const Color(0xff1D1617).withOpacity(0.11),
                 blurRadius: 40,
                 spreadRadius: 0.0,
               )
--- lib/src/screens/models/category_model.dart
+++ lib/src/screens/models/category_model.dart
@@ -10,4 +10,36 @@ class CategoryModel {
     required this.iconPath,
     required this.backgroundColor,
   });
+  List<CategoryModel> getCateGory() {
+    List<CategoryModel> categories = [];
+    categories.add(
+      CategoryModel(
+        itemName: 'Salad',
+        iconPath: 'assets/icons/orange-snacks.svg',
+        backgroundColor: const Color(0xff92A3FD),
+      ),
+    );
+    categories.add(
+      CategoryModel(
+        itemName: 'Salad',
+        iconPath: 'assets/icons/pancakes.svg',
+        backgroundColor: const Color(0xff92A3FD),
+      ),
+    );
+    categories.add(
+      CategoryModel(
+        itemName: 'Salad',
+        iconPath: 'assets/icons/pie.svg',
+        backgroundColor: const Color(0xff92A3FD),
+      ),
+    );
+    categories.add(
+      CategoryModel(
+        itemName: 'Salad',
+        iconPath: 'assets/icons/plate.svg',
+        backgroundColor: const Color(0xff92A3FD),
+      ),
+    );
+    return categories;
+  }
 }
